be3a6be09df4cd7dfd782d1cbfeda1100df24bb0
mkreive <mkreive@gmail.com>
2022-05-23 16:23:41 +0300

    gallery component done

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,29 +4,29 @@ function Gallery() {
     return (
         <div className="gallery">
             <img
-                className="gallery__picture gallery__picture-big"
+                className="gallery__picture-big"
                 src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1_ra55jy.jpg"
                 alt="product"
             ></img>
             <div className="gallery-thumbnails">
                 <img
-                    className="gallery__picture gallery__picture-small"
+                    className="gallery__picture-small selected"
                     src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1-thumbnail_qfqqjm.jpg"
                     alt="product"
                 ></img>
 
                 <img
-                    className="gallery__picture gallery__picture-small"
+                    className="gallery__picture-small"
                     src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-3-thumbnail_drkel7.jpg"
                     alt="product"
                 ></img>
                 <img
-                    className="gallery__picture gallery__picture-small"
+                    className="gallery__picture-small"
                     src="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/image-product-4-thumbnail_qcdzs1.jpg"
                     alt="product"
                 ></img>
                 <img
-                    className="gallery__picture gallery__picture-small"
+                    className="gallery__picture-small"
                     src="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/image-product-2-thumbnail_bbmf5h.jpg"
                     alt="product"
                 ></img>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,32 @@
 import "../index.scss";
 
 function Product() {
-    return <div className="product"></div>;
+    return (
+        <div className="product">
+            <h3 className="header-small">Sneaker company</h3>
+            <h1 className="header-big">Fall limited edition sneakers</h1>
+            <p className="text">
+                These low profile sneakersare your perfect casual wear
+                companion. Featuring adurable rubberouter sole they'll
+                withstandeverything theweather can offer.
+            </p>
+            <div className="price">
+                <div className="price__now">
+                    <span className="header-medium">$125.00</span>
+                    <span className="discount">50%</span>
+                </div>
+                <div className="price__before">$250.00</div>
+            </div>
+            <div className="addtocart">
+                <div className="addtocart__quantity">
+                    <span>-</span>
+                    <span>0</span>
+                    <span>+</span>
+                </div>
+                <button className="btn addtocart__btn">Add to cart</button>
+            </div>
+        </div>
+    );
 }
 
 export default Product;
