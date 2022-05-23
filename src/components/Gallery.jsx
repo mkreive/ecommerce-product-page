87b5ef82bbd7be15209a30828e970b90b05d2f30
mkreive <mkreive@gmail.com>
2022-05-23 15:08:19 +0300

    jsx renaming

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,29 +5,29 @@ function Gallery() {
         <div className="gallery">
             <img
                 className="gallery__picture gallery__picture-big"
-                scr="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1_ra55jy.jpg"
+                src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1_ra55jy.jpg"
                 alt="product"
             ></img>
             <div className="gallery-thumbnails">
                 <img
                     className="gallery__picture gallery__picture-small"
-                    scr="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1-thumbnail_qfqqjm.jpg"
+                    src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1-thumbnail_qfqqjm.jpg"
                     alt="product"
                 ></img>
 
                 <img
                     className="gallery__picture gallery__picture-small"
-                    scr="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-3-thumbnail_drkel7.jpg"
+                    src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-3-thumbnail_drkel7.jpg"
                     alt="product"
                 ></img>
                 <img
                     className="gallery__picture gallery__picture-small"
-                    scr="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/image-product-4-thumbnail_qcdzs1.jpg"
+                    src="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/image-product-4-thumbnail_qcdzs1.jpg"
                     alt="product"
                 ></img>
                 <img
                     className="gallery__picture gallery__picture-small"
-                    scr="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/image-product-2-thumbnail_bbmf5h.jpg"
+                    src="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/image-product-2-thumbnail_bbmf5h.jpg"
                     alt="product"
                 ></img>
             </div>
