8533ee368068d159aead82ac7a63bc985e77ebe5
mkreive <mkreive@gmail.com>
2022-05-24 09:49:04 +0300

    price component done

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,19 +3,19 @@ import "../index.scss";
 function Product() {
     return (
         <div className="product">
-            <h3 className="header-small">Sneaker company</h3>;
+            <h3 className="header-small">Sneaker company</h3>
             <h1 className="header-big">Fall limited edition sneakers</h1>
             <p className="text">
                 These low profile sneakersare your perfect casual wear
                 companion. Featuring adurable rubberouter sole they'll
                 withstandeverything theweather can offer.
             </p>
-            <div className="price">
-                <div className="price__now">
-                    <span className="header-medium">$125.00</span>
-                    <span className="discount">50%</span>
+            <div className="prices">
+                <div className="price">
+                    <span className="header-medium price-now">$125.00</span>
+                    <span className="price-discount">50%</span>
                 </div>
-                <div className="price__before">$250.00</div>
+                <div className="price-before">$250.00</div>
             </div>
             <div className="addtocart">
                 <div className="addtocart__quantity">
