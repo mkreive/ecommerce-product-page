fb1756a5afbf53bf63ad6a68124feb5fbce28461
mkreive <mkreive@gmail.com>
2022-05-24 14:42:29 +0300

    all products components done

---
+++ src/components/AllProducts.jsx
@@ -0,0 +1,16 @@
+function AllProducts() {
+    return (
+        <div className="all-products">
+            <h3 className="header-small">Sneaker company</h3>
+            <div className="gallery-thumbnails">
+                <img
+                    className="gallery__picture-small selected"
+                    src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1-thumbnail_qfqqjm.jpg"
+                    alt="product"
+                ></img>
+            </div>
+        </div>
+    );
+}
+
+export default AllProducts;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import "../index.scss";
 import Gallery from "./Gallery";
 import Product from "./Product";
+import AllProducts from "./AllProducts";
 
 function Main() {
     return (
         <div className="main">
             <Gallery></Gallery>
             <Product></Product>
+            {/* <AllProducts></AllProducts> */}
         </div>
     );
 }
