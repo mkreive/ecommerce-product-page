15fa04359b349960729ea83d6baace62e9a664f7
mkreive <mkreive@gmail.com>
2022-05-24 14:51:32 +0300

    reducing component files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,52 @@
 import "../index.scss";
-import Logo from "./Logo";
-import Navigation from "./Navigation";
-import Cart from "./Cart";
-import Account from "./Account";
 
 function Header() {
     return (
         <header className="header">
-            <Logo></Logo>
-            <Navigation></Navigation>
-            <Cart></Cart>
-            <Account></Account>
+            <img
+                className="logo"
+                src="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/logo_d2dxhh.svg"
+                alt="logo"
+            ></img>
+            <nav className="navigation">
+                <ul className="navigation__links">
+                    <li>
+                        <a className="navigation__links-link" href="#section-1">
+                            Collections
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navigation__links-link" href="#section-2">
+                            Men
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navigation__links-link" href="#section-2">
+                            Women
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navigation__links-link" href="#section-2">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a className="navigation__links-link" href="#section-2">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <img
+                className="cart"
+                src="https://res.cloudinary.com/kreiva/image/upload/v1653033614/FrontendMentor/EcommerceProductPage/icon-cart_tpxhw0.svg"
+                alt="cart"
+            ></img>
+            <img
+                className="account"
+                src="https://res.cloudinary.com/kreiva/image/upload/v1653033615/FrontendMentor/EcommerceProductPage/image-avatar_pvqg6m.png"
+                alt="user"
+            ></img>
         </header>
     );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,30 +2,66 @@ import "../index.scss";
 
 function Product() {
     return (
-        <div className="product">
-            <h3 className="header-small">Sneaker company</h3>
-            <h1 className="header-big">Fall limited edition sneakers</h1>
-            <p className="text">
-                These low profile sneakersare your perfect casual wear
-                companion. Featuring adurable rubberouter sole they'll
-                withstandeverything theweather can offer.
-            </p>
-            <div className="prices">
-                <div className="price">
-                    <span className="header-medium price-now">$125.00</span>
-                    <span className="price-discount">50%</span>
+        <main>
+            <div className="gallery">
+                <div className="gallery__arrows">
+                    <button className="gallery__arrows-left"></button>
+                    <button className="gallery__arrows-right"></button>
                 </div>
-                <div className="price-before">$250.00</div>
-            </div>
-            <div className="addtocart">
-                <div className="addtocart__quantity">
-                    <span className="addtocart__quantity-minus"></span>
-                    <span className="addtocart__quantity-count">0</span>
-                    <span className="addtocart__quantity-add"></span>
+                <img
+                    className="gallery__picture-big"
+                    src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1_ra55jy.jpg"
+                    alt="product"
+                ></img>
+                <div className="gallery-thumbnails">
+                    <img
+                        className="gallery__picture-small selected"
+                        src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-1-thumbnail_qfqqjm.jpg"
+                        alt="product"
+                    ></img>
+
+                    <img
+                        className="gallery__picture-small"
+                        src="https://res.cloudinary.com/kreiva/image/upload/v1653033616/FrontendMentor/EcommerceProductPage/image-product-3-thumbnail_drkel7.jpg"
+                        alt="product"
+                    ></img>
+                    <img
+                        className="gallery__picture-small"
+                        src="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/image-product-4-thumbnail_qcdzs1.jpg"
+                        alt="product"
+                    ></img>
+                    <img
+                        className="gallery__picture-small"
+                        src="https://res.cloudinary.com/kreiva/image/upload/v1653033617/FrontendMentor/EcommerceProductPage/image-product-2-thumbnail_bbmf5h.jpg"
+                        alt="product"
+                    ></img>
                 </div>
-                <button className="btn addtocart__btn">Add to cart</button>
             </div>
-        </div>
+            <div className="product">
+                <h3 className="header-small">Sneaker company</h3>
+                <h1 className="header-big">Fall limited edition sneakers</h1>
+                <p className="text">
+                    These low profile sneakersare your perfect casual wear
+                    companion. Featuring adurable rubberouter sole they'll
+                    withstandeverything theweather can offer.
+                </p>
+                <div className="prices">
+                    <div className="price">
+                        <span className="header-medium price-now">$125.00</span>
+                        <span className="price-discount">50%</span>
+                    </div>
+                    <div className="price-before">$250.00</div>
+                </div>
+                <div className="addtocart">
+                    <div className="addtocart__quantity">
+                        <span className="addtocart__quantity-minus"></span>
+                        <span className="addtocart__quantity-count">0</span>
+                        <span className="addtocart__quantity-add"></span>
+                    </div>
+                    <button className="btn addtocart__btn">Add to cart</button>
+                </div>
+            </div>
+        </main>
     );
 }
 
