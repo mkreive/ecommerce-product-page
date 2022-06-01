0cf2d47c1376bb666d476059c246770441c10ddf
mkreive <mkreive@gmail.com>
2022-06-01 10:59:22 +0300

    starting react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { useReducer, useState, useNavigation } from "react";
-
 import "./index.scss";
 
 import Header from "./components/Header";
-import Collections from "./components/Collections";
-import AllProducts from "./components/AllProducts";
 import Product from "./components/Product";
 
 const App = function () {
-    const [main, setMain] = useState(<Collections></Collections>);
-
-    const navigationClickHandler = function (clickedBtn) {
-        setMain(<Product />);
-    };
     return (
         <div className="App">
-            <Header onNavigationClick={navigationClickHandler}></Header>
-            {main}
+            <Header />
+            <Product />
         </div>
     );
 };
