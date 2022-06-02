cdc87c150036b9365fe53a3f1863aa5d98e93218
mkreive <mkreive@gmail.com>
2022-06-02 14:53:23 +0300

    is loading state done

--- src/components/Men.jsx
+++ src/components/Men.jsx
@@ -4,17 +4,29 @@ import ProductItem from "./ProductItem";
 
 const Men = function () {
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         const getProducts = async function () {
             const fetchedData = await fetchProducts();
             const allProducts = Object.values(fetchedData);
             const menProducts = menFilter(allProducts);
             setProducts(menProducts);
+            setIsLoading(false);
         };
         getProducts();
     }, []);
 
+    if (isLoading) {
+        return (
+            <main className="main">
+                <div className="gallery__shop">
+                    <h1 className="header-small">Loading... </h1>
+                </div>
+            </main>
+        );
+    }
     return (
         <main className="main">
             <div className="gallery__shop">
--- src/components/Women.jsx
+++ src/components/Women.jsx
@@ -4,16 +4,29 @@ import ProductItem from "./ProductItem";
 
 const Women = function () {
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         const getProducts = async function () {
             const fetchedData = await fetchProducts();
             const allProducts = Object.values(fetchedData);
             const womenProducts = ladyFilter(allProducts);
             setProducts(womenProducts);
+            setIsLoading(false);
         };
         getProducts();
     }, []);
+
+    if (isLoading) {
+        return (
+            <main className="main">
+                <div className="gallery__shop">
+                    <h1 className="header-small">Loading... </h1>
+                </div>
+            </main>
+        );
+    }
 
     return (
         <main className="main">
